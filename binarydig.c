06df03f6524b071f8647427c08c9a5079584ca0a
PavithraV <pavithradeepa96@gmail.com>
2016-01-24 21:13:45 +0530

    binarydig.c

--- binarydig.c
+++ binarydig.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 int main() 
 {
-		int decinm=8023,quot,i=0,binnum[100],count;
-		quot=decinm;
+		int decinm,num,i=0,binnum[100],count;
+		printf("Enter the decimal number\n");
+		scanf("%d",&decinm);
+		num=decinm;
 		while(quot!=0)
 		{
-				binnum[i++]=quot%2;
-				quot=quot/2;
+				binnum[i++]=num%2;
+				num=num/2;
 		}
 		count=i;
 		printf("the number of bits= %d",count);
